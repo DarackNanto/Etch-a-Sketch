6cc27eb11203745fac87d032603a732acf3fc75d
Darack <103718989+DarackNanto@users.noreply.github.com>
2023-01-06 08:30:00 -0600

    Create hover effect. Changes grid color from white to red with mouse over.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 //modify the DOM to add grids
-const container = document.querySelector("#container");
-const grids = document.createElement("div");
+const grid = document.querySelector("#container");
 
 //call the function
 createGrid();
+addEventListener();
 
-
-//Function to create grid 
+//Function to create grid by default is create a 16 * 16
 function createGrid() {
-  for (let i = 1; i <= 20; i++) {
-    const grids = document.createElement("div");
-    
-    
-    
-    grids.setAttribute("id", `grid${i}`);
+    for (let i = 1; i <= 16 * 16; i++) {
+      const gridElement = document.createElement("div");
+      gridElement.setAttribute("id", `grid-element`);
+      container.appendChild(gridElement);
+    }
+}
 
-    container.appendChild(grids);
-
-
+//Create event listener from an array of NodeList then call the change color func
+function addEventListener() {
+  let list = document.querySelectorAll("div#grid-element");
+  console.log(list);
+  console.log(list[1]);
+  console.log(list.length);
+  for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("mouseover", changeColor, false);
   }
+}
+// Function responsible of changing color from black to red
+function changeColor() {
+  this.style.backgroundColor = "red";
 }
