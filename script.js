26e0c5b542b3fff3b0afb1232469aeeabe005e61
Darack <103718989+DarackNanto@users.noreply.github.com>
2023-01-10 15:12:51 -0600

    Create button to modify grid element The user can enter a number of desire grig from 1 - 100 and the grid will be adjusted dynamically. Adjust the grid to stay within the container 500px square.

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
+//Hold user input number for grid - default is 16 * 16
+var inputNumber = 16;
+
 //modify the DOM to add grids
 const grid = document.querySelector("#container");
 
+createGrid(inputNumber);
+
+// prompt user to enter a desired grid number between 1 - 100 - default 16 * 16.
+document.querySelector("#numberBtn").addEventListener("click", promptUser);
+function promptUser() {
+  let gridUserInput;
+  //enforce that input is less that 100.
+  do {
+    gridUserInput = prompt("Please enter your grid number between 1 and 100", "16");
+    inputNumber = Number(gridUserInput);
+    console.log(inputNumber);
+  }
+  while (inputNumber > 100)
 //call the function
-createGrid();
-addEventListener();
+clearGrid();
+createGrid(inputNumber);
+}
 
 //Function to create grid by default is create a 16 * 16
-function createGrid() {
-    for (let i = 1; i <= 16 * 16; i++) {
+function createGrid(a) {
+
+    for (let i = 1; i <= a * a; i++) {
       const gridElement = document.createElement("div");
       gridElement.setAttribute("id", `grid-element`);
       container.appendChild(gridElement);
+      //adding css style and passing the cols and rows number
+      grid.style.gridTemplateColumns = `repeat(${a}, 1fr)`;
+      grid.style.gridTemplateRows = `repeat(${a}, 1fr)`;
     }
+    addEventListener();
+}
+
+//clear previous DOM grid data
+function clearGrid() {
+  while (grid.firstChild){
+    grid.removeChild(grid.firstChild);
+  }
 }
 
 //Create event listener from an array of NodeList then call the change color func
@@ -28,3 +57,4 @@ function addEventListener() {
 function changeColor() {
   this.style.backgroundColor = "red";
 }
+
